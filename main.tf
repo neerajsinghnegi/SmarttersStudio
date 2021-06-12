da0679bdb362c1c6b0ba894fe47db08602e0aa3e
Neeraj Singh Negi <negisneeraj23@gmail.com>
2021-06-12 22:49:32 +0530

    demo new

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ terraform {
 }
 
 provider "aws" {
-  access_key = ${{secrets.AWS_ACCESS_KEY_ID}}
-  secret_key = ${{secrets.AWS_SECRET_ACCESS_KEY}}
+  aws-access-key-id = ${{secrets.AWS_ACCESS_KEY_ID}}
+  aws-secret-access-key = ${{secrets.AWS_SECRET_ACCESS_KEY}}
   region  = "ap-south-1"
 }
 
