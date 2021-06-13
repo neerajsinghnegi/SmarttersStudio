b044116983072695e1aceea283589dfa2d3a372a
neerajsinghnegi <60294234+neerajsinghnegi@users.noreply.github.com>
2021-06-13 12:23:22 +0530

    Update main.tf
    added new

--- main.tf
+++ main.tf
@@ -12,3 +12,11 @@ resource "null_resource" "example" {
        value = "A example resource that does nothing!"
        }
     }
+resource "aws_instance" "app_server" {
+  ami           = "ami-0ad704c126371a549"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Demo"
+  }
+}
