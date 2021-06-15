da77fffd5519bf280510b2f7dc8ad07ce9f02c44
Neeraj Singh Negi <60294234+neerajsinghnegi@users.noreply.github.com>
2021-06-15 15:46:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,22 @@
 terraform {
-       backend "remote" {         
+  resource "aws_instance" "app_server" {
+  ami           = "ami-0ad704c126371a549"
+  instance_type = "t2.micro"
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  backend "remote" {         
        organization = "neerajsinghnegi"
        workspaces {
        name = "Github-Action"
        }
      }
   }
-
-resource provider "aws" {
-       version = "~> 3.0"
-}
-
-resource "null_resource" "example" {
-     triggers = {
-       value = "A example resource that does nothing!"
-       }
-    }
-resource "aws_instance" "app_server" {
-  ami           = "ami-0ad704c126371a549"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Demo-01"
-  }
-}
