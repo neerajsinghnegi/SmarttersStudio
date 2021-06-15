ab6f3ef058d7ea70d6567672c065cb6e045eecd2
Neeraj Singh Negi <60294234+neerajsinghnegi@users.noreply.github.com>
2021-06-15 14:44:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
   }
 
 provider "aws" {
-       access_key = "${var.AWS_ACCESS_KEY_ID}"
-       secret_key = "${var.AWS_SECRETE_ACCESS_KEY}"
+       AWS_ACCESS_KEY_ID = "${var.AWS_ACCESS_KEY_ID}"
+       AWS_SECRETE_ACCESS_KEY = "${var.AWS_SECRETE_ACCESS_KEY}"
        region = "ap-south-1"
 }
 
