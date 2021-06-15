6c2f1dd10131a39f87907647a950e13e3d0e8e4e
Neeraj Singh Negi <60294234+neerajsinghnegi@users.noreply.github.com>
2021-06-15 15:01:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,17 +7,11 @@ terraform {
      }
   }
 
-provider "aws" {
-       aws_access_key_id = "${secrets.AWS_ACCESS_KEY_ID}"
-       aws_secret_access_key = "${secrets.AWS_SECRETE_ACCESS_KEY}"
-       region = "ap-south-1"
-}
-
-#resource "null_resource" "example" {
- #    triggers = {
-   #    value = "A example resource that does nothing!"
-    #   }
-    #}
+resource "null_resource" "example" {
+     triggers = {
+       value = "A example resource that does nothing!"
+       }
+    }
 resource "aws_instance" "app_server" {
   ami           = "ami-0ad704c126371a549"
   instance_type = "t2.micro"
